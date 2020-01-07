2fc212cb89fb3f60ed8995dc152c4caf172a40db
Hilmar Gústafsson <hilmargustafs@gmail.com>
2020-01-07 11:31:35 +0100

    Format

--- src/mappers/mod.rs
+++ src/mappers/mod.rs
@@ -2,6 +2,6 @@ mod degree_centrality;
 mod identity;
 mod pagerank;
 
+pub use degree_centrality::DegreeCentralityMapper;
 pub use identity::IdentityMapper;
 pub use pagerank::PageRankMapper;
-pub use degree_centrality::DegreeCentralityMapper;
